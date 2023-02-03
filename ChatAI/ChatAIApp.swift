8b225f925a135f78b3c87f1c50725c351e724b82
Pavel Neprin <41507524+neprin@users.noreply.github.com>
2023-02-03 18:17:50 +0300

    Initial Commit

---
+++ ChatAI/ChatAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAIApp.swift
+//  ChatAI
+//
+//  Created by Pavel Neprin on 2/3/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
